07c9a7e3e99cc38c59c8fc56e616538f6caecb4e
ITsPOLY <ukolova_p@inbox.ru>
2023-05-10 02:49:05 +0300

    Нужно больше функций (исправления)

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
-let provideRandomInt = function (bottom, top) {
+const provideRandomInt = function (bottom, top) {
   if (bottom < 0 || top < 0) {
-    console.log('Допустимо использование только положительных чисел, включая 0');
     return 0;
   }
 
   if (bottom > top) {
-    console.log('Верхняя граница top диапазона должна быть больше нижней bottom');
     return 0;
   }
 
   return Math.floor(Math.random() * (top - bottom) + bottom);
 }
 
-let checkMakLength = function (string, maximumLength) {
+const checkMaxLength = function (string, maximumLength) {
   if (string.length <= maximumLength) {
     return true;
   }
 
   return false;
 }
+
+provideRandomInt(0, 9);
+checkMaxLength('cat', 5)
