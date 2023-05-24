8a59c054823febf558a97b857a406f22d98f5dd0
ITsPOLY <ukolova_p@inbox.ru>
2023-05-24 22:11:50 +0300

    Отрисуй меня полностью

---
+++ js/drawMe.js
@@ -0,0 +1,20 @@
+const picturePattern = document.querySelector('#picture');
+const block = document.querySelector('.pictures');
+
+const drawPicture = (elements) => {
+  const pictureFragment = document.createDocumentFragment();
+  elements.forEach((element) => {
+    const pattern = picturePattern.cloneNode(true).content;
+    const img = pattern.querySelector('.picture__img');
+    const likes = pattern.querySelector('.picture__likes');
+    const comments = pattern.querySelector('.picture__comments');
+
+    img.src = element.url;
+    likes.textContent = element.likes;
+    comments.textContent = element.comments;
+
+    pictureFragment.appendChild(pattern);
+  });
+
+  block.appendChild(pictureFragment);
+};
