64ad7a2103c1186f345d6c05c744b1f1688082ca
ITsPOLY <ukolova_p@inbox.ru>
2023-05-24 20:43:59 +0300

    Changed

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ function generatePhotos() {
   const arrayOfData = [];
   for (let i = 1; i <= 25; i++) {
     const id = i;
-    const url = 'photos/' + i + '.jpg';
+    const url = 'photos/${i}.jpg';
     const description = 'Какой прекрасный закат!';
     const likes = provideRandomInt(15, 200);
     const comments = provideRandomInt(0, 200);
@@ -38,7 +38,7 @@ function generatePhotos() {
       comments
     };
     arrayOfData[i - 1] = photos;
-  };
+  }
 };
 
 generatePhotos(15);
